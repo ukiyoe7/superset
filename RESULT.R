307b33212a1ccdd48a5e61566c7807211ce95019
unknown <sandro007@gmail.com>
2023-11-21 10:49:13 -0300

    OK

--- RESULT.R
+++ RESULT.R
@@ -8,7 +8,7 @@ library(stringr)
 
 con2 <- dbConnect(odbc::odbc(), "repro",encoding="Latin1")
 
-# RESULT
+## RESULT ===============================================
 
 new_result <- dbGetQuery(con2, statement = read_file('RESULT.sql')) 
 
@@ -53,12 +53,13 @@ result_geral_baixa <-
 new_result2 <-
 union_all(result_setores_emissao,result_geral_emissao) %>% 
    union_all(.,result_setores_baixa) %>% 
-     union_all(.,result_geral_baixa)
+     union_all(.,result_geral_baixa) %>% 
+      mutate(INDICADOR='RESULTADO MES')
 
 View(new_result2)
 
 
-## METAS
+## METAS ==================================================
 
 METAS_2023 <-
 get(load("C:\\Users\\REPRO SANDRO\\Documents\\R\\RESULT\\BASES\\METAS_2023.RData"))
@@ -71,23 +72,56 @@ METAS_2023 %>% filter(SETOR=="GERAL") %>% summarize(V=sum(VALOR))
 
 save(METAS_2023,file = "C:\\Users\\REPRO SANDRO\\Documents\\R\\RESULT\\BASES\\METAS_2023.RData")
 
-
-metas_setores <-
+## EMISSAO
+metas_setores_emissao <-
   METAS_2023 %>%  
    filter(DATA==floor_date(Sys.Date(),"month")) %>% 
     group_by(MES=floor_date(DATA,"month"),SETOR) %>% 
-     summarize(METAS=sum(VALOR)) %>% as.data.frame() %>% select(-MES)
+     summarize(VALOR=sum(VALOR)) %>% as.data.frame() %>%
+      mutate(TIPO='EMISSAO') %>% 
+       mutate(INDICADOR='META MES ATUAL') 
 
 
 View(metas_setores)
 
 
+metas_setores_baixa <-
+  METAS_2023 %>%  
+  filter(DATA==floor_date(Sys.Date(),"month")) %>% 
+  group_by(MES=floor_date(DATA,"month"),SETOR) %>% 
+  summarize(VALOR=sum(VALOR)) %>% as.data.frame() %>%
+  mutate(TIPO='BAIXA') %>% 
+  mutate(INDICADOR='META MES ATUAL') 
+
+
+metas_setores <-
+   union_all(metas_setores_emissao,metas_setores_baixa)
+
+
+View(metas_setores)
+
+## ALACANCE ============================================
+
 alcance_result <-
-left_join(new_result2,metas_setores ,by="SETOR") %>% mutate(ALCANCE=round(((VALOR/METAS)*100),2))
+left_join(new_result2,metas_setores,by="SETOR") %>% 
+   mutate(ALCANCE=round(((VALOR/METAS)*100),2))
 
 
 View(alcance_result)
 
 
+
+## BASE GERAL =========================================
+
+corder <- c("MES","SETOR","TIPO","INDICADOR","VALOR")
+
+base_result <-
+union_all(
+new_result2 %>% .[,corder],
+
+metas_setores %>% .[,corder])
+
+
+View(base_result)
 
 
