ae3bad4d760ad30fcf60b36bc57f68c9dffdc6a7
unknown <sandro007@gmail.com>
2023-11-21 15:19:11 -0300

    ok

--- RESULT.R
+++ RESULT.R
@@ -1,4 +1,7 @@
 ## NEW RESULT SUPERSET
+## 11.2023
+## SANDRO JAKOSKA
+
 
 library(DBI)
 library(dplyr)
@@ -8,7 +11,11 @@ library(stringr)
 
 con2 <- dbConnect(odbc::odbc(), "repro",encoding="Latin1")
 
+
+
 ## RESULT ===============================================
+
+
 
 new_result <- dbGetQuery(con2, statement = read_file('RESULT.sql')) 
 
@@ -82,7 +89,7 @@ metas_setores_emissao <-
        mutate(INDICADOR='META MES ATUAL') 
 
 
-View(metas_setores)
+View(metas_setores_emissao)
 
 
 metas_setores_baixa <-
@@ -100,14 +107,62 @@ metas_setores <-
 
 View(metas_setores)
 
-## ALACANCE ============================================
+
+
+## ALCANCE ============================================
 
 alcance_result <-
-left_join(new_result2,metas_setores,by="SETOR") %>% 
-   mutate(ALCANCE=round(((VALOR/METAS)*100),2))
+ left_join(new_result2,
+            METAS_2023 %>%  
+             filter(DATA==floor_date(Sys.Date(),"month")) %>% 
+              group_by(MES=floor_date(DATA,"month"),SETOR) %>% 
+               summarize(METAS=sum(VALOR)) %>% 
+                as.data.frame() %>% select(-MES),by="SETOR") %>% 
+                 mutate(ALCANCE=round(((VALOR/METAS)*100),2)) %>% 
+                  select(-VALOR,-METAS) %>% 
+                   mutate(INDICADOR='ALCANCE') %>% 
+                    rename(VALOR=ALCANCE)
 
 
 View(alcance_result)
+
+
+
+
+## ESPERADO  ============================================
+
+## CALC DIAS UTEIS
+
+first_day <- floor_date(Sys.Date(), unit = "month")
+
+last_day <- ceiling_date(Sys.Date(), 'month') - days(1)
+
+num_days <- as.numeric(days_in_month(first_day))
+
+num_weekends <- sum(wday(seq(first_day, last_day, by = "day")) %in% c(1,7))
+
+holidays <- data.frame(DATA=c(as.Date('2023-10-12'),
+                              as.Date('2023-11-02'),
+                              as.Date('2023-11-15'),
+                              as.Date('2023-12-25'),
+                              as.Date('2023-12-31')
+                              )) %>% 
+                              mutate(MES=floor_date(DATA, unit = "month")) %>% 
+                              filter(MES==floor_date(Sys.Date(), unit = "month")) %>% 
+                              tally()
+                              
+                      
+
+num_days = (num_days - num_weekends) - holidays 
+
+days_until_yesterday <- length(seq(first_day,Sys.Date(),by = "day"))-1
+
+## 
+
+
+metas_setores %>% 
+  mutate(ALCANCE_ESPERADO=(VALOR/as.numeric(num_days))*length(seq(first_day,Sys.Date(),by = "day"))-1) %>% View()
+
 
 
 
@@ -119,7 +174,11 @@ base_result <-
 union_all(
 new_result2 %>% .[,corder],
 
-metas_setores %>% .[,corder])
+metas_setores %>% .[,corder]) %>% 
+  
+union_all(. ,  
+
+alcance_result  %>% .[,corder])  
 
 
 View(base_result)
