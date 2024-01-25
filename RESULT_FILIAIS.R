46ae38614d739d2502a9a264436a854cfa256d1f
unknown <sandro007@gmail.com>
2024-01-25 13:29:46 -0300

    OK

--- RESULT_FILIAIS.R
+++ RESULT_FILIAIS.R
@@ -21,16 +21,11 @@ con_spset <- dbConnect(odbc::odbc(), "dbcomercial", encoding = "UTF-8")
 result_filiais <- dbGetQuery(con_sgo, statement = read_file("C:\\Users\\REPRO SANDRO\\Documents\\R\\SUPERSET\\SQL\\RESULT_FILIAIS.sql")) 
 
 
-View(result_filiais)
-
 
 ## METAS ==================================================
 
-METAS_FILIAIS_2023 <-
-  get(load("C:\\Users\\REPRO SANDRO\\Documents\\R\\RESULT\\BASES\\METAS_FILIAIS_2023.RData"))
-
-
-save(METAS_FILIAIS_2023,file = "C:\\Users\\REPRO SANDRO\\Documents\\R\\RESULT\\BASES\\METAS_FILIAIS_2023.RData")
+METAS_FILIAIS_2024 <-
+  get(load("C:\\Users\\REPRO SANDRO\\Documents\\R\\RESULT\\BASES\\METAS_FILIAIS_2024.RData"))
 
 
 ## RESULT =======================================================
@@ -40,29 +35,25 @@ result_filiais %>%
   group_by(MES=floor_date(DATA,"month"),EMPCODIGO) %>% 
   summarize(VALOR=sum(VRLIQUIDO)) %>% mutate(INDICADOR='VENDA LIQUIDA')
 
-View(result_filiais2)
 
 
 ## METAS =======================================================
 
-metas_filiais_2023_2 <-
-  METAS_FILIAIS_2023 %>% 
-  filter(DATA==floor_date(Sys.Date(),"month")-years(1)) %>% 
+metas_filiais_2024_2 <-
+  METAS_FILIAIS_2024 %>% 
+  filter(DATA==floor_date(Sys.Date(),"month")) %>% 
   group_by(MES=floor_date(DATA,"month"),EMPCODIGO) %>% 
-  summarize(VALOR=sum(VALOR)) %>% as.data.frame() %>%
+  summarize(METAS=sum(METAS)) %>% as.data.frame() %>%
   mutate(INDICADOR='META MES ATUAL') 
-
-View(metas_filiais_2023_2)
 
 
 ## ALCANCE  ============================================
 
 alcance_result_filiais <-
   left_join(result_filiais_2,
-            METAS_FILIAIS_2023 %>% 
-             rename(METAS=VALOR) %>% 
-              filter(DATA==floor_date(Sys.Date(),"month")-years(1)) %>% 
-               select(-EMPRESA,-DATA),by="EMPCODIGO") %>% 
+            METAS_FILIAIS_2024 %>% 
+              filter(DATA==floor_date(Sys.Date(),"month")) %>% 
+               select(EMPCODIGO,-DATA,METAS),by="EMPCODIGO") %>% 
   mutate(ALCANCE=round(((VALOR/METAS)),3)) %>% 
   select(-VALOR,-METAS) %>% 
   mutate(INDICADOR='ALCANCE') %>% 
@@ -134,28 +125,28 @@ days_until_yesterday <- days_mtd_no_holidays - num_weekends_mtd
 ## alcance esperado
 
 result_esperado_filiais <-
-  metas_filiais_2023 %>% 
-   filter(DATA==floor_date(Sys.Date(),"month")-years(1)) %>% 
-    mutate(ALCANCE_ESPERADO=(VALOR/as.numeric(num_days2))) %>% 
+  metas_filiais_2024_2 %>% 
+   filter(MES==floor_date(Sys.Date(),"month")) %>% 
+    mutate(ALCANCE_ESPERADO=(METAS/as.numeric(num_days2))) %>% 
      mutate(ALCANCE_ESPERADO2=(ALCANCE_ESPERADO*as.numeric(days_until_yesterday))) %>% 
-      mutate(ALCANCE_ESPERADO3=(ALCANCE_ESPERADO2/VALOR)) %>% 
+      mutate(ALCANCE_ESPERADO3=(ALCANCE_ESPERADO2/METAS)) %>% 
        mutate(INDICADOR='ALCANCE ESPERADO') %>% 
-        select(EMPCODIGO,DATA,ALCANCE_ESPERADO3,INDICADOR) %>% 
-         rename(MES=DATA) %>% 
+        select(EMPCODIGO,MES,ALCANCE_ESPERADO3,INDICADOR) %>% 
           rename(VALOR=ALCANCE_ESPERADO3)
 
-View(result_esperado_filiais)
 
 
 ## VAR ESPERADO  ============================================
 
 
-left_join(alcance_result_filiais %>% 
+var_esperado_filiais <- 
+ left_join(alcance_result_filiais %>% 
             select(-INDICADOR),result_esperado_filiais %>% 
-            select(-INDICADOR,-MES),by="EMPCODIGO") %>% 
+             select(-INDICADOR,-MES),by="EMPCODIGO") %>% 
               as.data.frame() %>% 
-              mutate(VAR=round(VALOR.x/VALOR.y-1,3)) %>% View()
-
+               mutate(VALOR=round(VALOR.x/VALOR.y-1,3)) %>%
+                mutate(INDICADOR='VAR ALCANCE ESPERADO') %>% 
+                 select(MES,EMPCODIGO,VALOR,INDICADOR) 
 
 
 ## BASE GERAL =========================================
@@ -166,7 +157,7 @@ dt_filiais <-
   union_all(
     result_filiais_2 %>% .[,corder],
     
-    metas_filiais_2023_2 %>% .[,corder]) %>% 
+    metas_filiais_2024_2 %>% rename(VALOR=METAS) %>% .[,corder]) %>% 
   
   union_all(. ,  
             
@@ -175,12 +166,63 @@ dt_filiais <-
   union_all(. ,  
             
             result_esperado_filiais %>% .[,corder]) %>%
+  
+  union_all(. ,  
+            
+            var_esperado_filiais %>% .[,corder]) %>%
+  
+  rename(FILIAL=EMPCODIGO) %>% mutate(FILIAL=case_when(
+    FILIAL==1 ~ 'MATRIZ',
+    FILIAL==3 ~ 'JOINVILLE',
+    FILIAL==4 ~ 'CRICIUMA',
+    FILIAL==5 ~ 'CHAPECO',
+    FILIAL==8 ~ 'BC',
+    TRUE ~ as.character(FILIAL)
+    )) %>% 
     
-  as.data.frame()
+  as.data.frame() 
 
 
 dt_filiais <- dt_filiais %>% mutate(MES=as.character(MES))
 
-dt_filiais <- dt_filiais %>% mutate(ID=seq(1:nrow(dt_filiais)))
+dt_filiais <- dt_filiais %>% mutate(ID=seq(1:nrow(dt_filiais))) 
+
 
 view(dt_filiais)
+
+
+## INSERT BANCO SUPERSET =========================================
+
+# Se não houver dados, interromper a execução
+if (nrow(dt_filiais) == 0) {
+  cat("Nenhum dado para inserir.\n")
+} else {
+  # Verificar se a tabela existe no MariaDB
+  nome_tabela <- "result_filiais"
+  existe_tabela <- dbExistsTable(con_spset, nome_tabela)
+  # Se a tabela não existir, criar automaticamente no MariaDB
+  if(!existe_tabela) {
+    dbWriteTable(con_spset, nome_tabela, dt_filiais, create = TRUE, row.names = FALSE)
+    # Alterar a codificação da tabela para utf8mb4
+    query_alteracao <- paste("ALTER TABLE", nome_tabela, 
+                             "CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;")
+    dbExecute(con_spset, query_alteracao)
+  } else { # Se a tabela já existir, inserir os dados
+    print(dt_filiais)
+    #container
+    y <- data.frame(MES=NA,FILIAL=NA,INDICADOR=NA,VALOR=NA,ID=NA)
+    for (i in 1:nrow(dt_filiais)) {
+      y[i,] <- dt_filiais[i,]
+      query <- paste("INSERT IGNORE INTO ",nome_tabela," (MES,FILIAL,INDICADOR,VALOR,ID) VALUES ('",y[i,"MES"],"','",y[i,"FILIAL"],"','",y[i,"INDICADOR"],"',",y[i,"VALOR"],",",y[i,"ID"],") ON DUPLICATE KEY UPDATE ID=",y[i,"ID"],",MES='",y[i,"MES"],"',FILIAL='",y[i,"FILIAL"],"',INDICADOR='",y[i,"INDICADOR"],"',VALOR=",y[i,"VALOR"],";", dbcomercial = "")
+      
+      dbSendQuery(con_spset,query)
+    }
+  }
+}
+
+
+
+
+# Fechar conexões
+dbDisconnect(con_spset)
+dbDisconnect(con_sgo)
